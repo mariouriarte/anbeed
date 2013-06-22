2bd139f41bd5d2b579d3257789e9451ef188003b
mario <mario.uriarte@gmail.com>
2013-06-22 18:12:30 -0400

    Inicio de estilos en templates

--- apps/farmaceutica/templates/layout.php
+++ apps/farmaceutica/templates/layout.php
@@ -1,14 +1,70 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
-  </head>
-  <body>
-    <?php echo $sf_content ?>
-  </body>
+    <head>
+        <title>
+            <?php if (!include_slot('title')): ?>
+                ANBEED
+            <?php endif ?>
+        </title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <?php $sf_user->setCulture('es') ?>
+        <?php include_http_metas() ?>
+        <?php include_metas() ?>
+        <?php include_stylesheets() ?>
+        <?php include_javascripts() ?>
+    </head>
+    <body>
+        <div id="global_domain_bar">
+            <div id="menu">
+                <?php if ($sf_user->isAuthenticated()): ?>
+                    
+                <?php endif; ?>
+            </div>
+        </div>
+        <div id="container">
+            
+            <div id="conten-header">
+
+                <div id="header">
+                    <div id="header-content">
+                        <div id="header-top">
+                            <div id="header-logo">
+                                <div id="logo">
+                                    
+                                </div>
+                                <?php if ($sf_user->isAuthenticated()): ?>
+                                <div id="info-user">
+                                    <h4>Usuario: <?php echo $sf_user->getUsername(); ?></h4>
+                                    <h3><?php echo link_to('Cerrar session', '@sf_guard_signout',
+                                                  array('id' => 'boton-salir')) 
+                                        ?>
+                                    </h3>
+                                </div>
+                                <?php endif; ?>
+                            </div>
+                            <div id="header-tools">
+                                <?php if (!include_slot('header-tools')): ?>
+                                esapcio 1
+                                <?php endif ?>
+                                <?php if (!$sf_user->isAuthenticated()): ?>
+                                    <h2>ANBEED SRL</h2>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div id="header-bottom">
+                           esapacio 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="nav_page">
+                <?php include_slot('navegador') ?>
+            </div>
+            <div id="content">
+                <?php echo $sf_content ?>
+                
+            </div>
+        </div>
+    </body>
 </html>
--- apps/portal/modules/inicio/templates/indexSuccess.php
+++ apps/portal/modules/inicio/templates/indexSuccess.php
@@ -1,1 +1,120 @@
-<a href="/farmaceutica_dev.php/empresas"><span>Empresas</span></a>
+<h1>Portal del Sistema, Bienvenido</h1>
+<div class="portal">
+    <div class="linea">
+        <div class="columna">
+            <div class="cubo">
+                <div class="adentro">
+                    <h2 class="titulo">Empresas</h2>
+                    <div class="contenido">
+                        <img src="/images/04_maps_32.png" />
+                        <ul>
+                            <li><a href="/farmaceutica_dev.php/empresas"><span>Empresas</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="columna">
+            <div class="cubo">
+                <div class="adentro">
+                    <h2 class="titulo">Laboratorios</h2>
+                    <div class="contenido">
+                        <img src="/images/applications_engineering.png" />
+                        <ul>
+                            <li><a href="/farmaceutica_dev.php/laboratorios"><span>Laboratorios Fabricantes</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php if ($sf_user->hasCredential('admin')): ?>
+            <div class="columna">
+                <div class="cubo">
+                    <div class="adentro">
+                        <h2 class="titulo">Paises</h2>
+                        <div class="contenido">
+                            <img src="/images/portal/configure.png">
+                            <ul>
+                                <li><a href="/farmaceutica_dev.php/paises"><span>Paises</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif ?>
+        <div class="columna">
+            <div class="cubo">
+                <div class="adentro">
+                    <h2 class="titulo">Cuenta</h2>
+                    <div class="contenido">
+                        <img src="/images/password.png" />
+                        <ul>
+                            <?php if ($sf_user->isAuthenticated()): ?>
+                                <li><a href="<?php echo url_for('@sf_guard_signout') ?>"><span>Salir</span></a></li>
+                            <?php else: ?>
+                                <li><a href="<?php echo url_for('@sf_guard_signin') ?>"><span>Ingresar</span></a></li>
+                            <?php endif ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="linea">
+        <div class="columna">
+            <div class="cubo">
+                <div class="adentro">
+                    <h2 class="titulo">Usuarios</h2>
+                    <div class="contenido">
+                        <img src="/images/applications_engineering.png" />
+                        <ul>
+                            <li><a href="/adm_dev.php/usuarios"><span>Usuarios</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="columna">
+            <div class="cubo">
+                <div class="adentro">
+                    <h2 class="titulo">Personas</h2>
+                    <div class="contenido">
+                        <img src="/images/applications_engineering.png" />
+                        <ul>
+                            <li><a href="/adm_dev.php/personas"><span>Personas</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div class="columna">
+            <div class="cubo">
+                <div class="adentro">
+                    <h2 class="titulo">otros</h2>
+                    <div class="contenido">
+                        <img src="/images/book2.png" />
+                        <ul>
+                            <li><a href="#">otros</a></li>              
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="columna">
+            <div class="cubo">
+                <div class="adentro">
+                    <h2 class="titulo">otros</h2>
+                    <div class="contenido">
+                        <img src="/images/book2.png" />
+                        <ul>
+                            <li><a href="#">otros</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
