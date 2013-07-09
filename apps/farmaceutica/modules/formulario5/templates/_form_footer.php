7a580dd78ef9d6c0c904af34e11713d8def7268b
mario <mario.uriarte@gmail.com>
2013-07-09 00:49:04 -0400

    Hoja de solicitud de registro de medicamento

---
+++ apps/farmaceutica/modules/formulario5/templates/_form_footer.php
@@ -0,0 +1,113 @@
+<h2>II. Datos de la Empresa Solicitante</h2>
+<div>
+    <table class="tabla-info-empresa">
+        <tbody>
+            <tr>
+                <th>Razón Social</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>R.S.N.</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Nombre del Titular</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Dirección</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Farmaceútico acreditado</th>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<h2>III. Datos del Laboratorio Fabricante</h2>
+<div>
+    <table class="tabla-info-empresa">
+        <tbody>
+            <tr>
+                <th>Laboratorio fabricante</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Bajo licencia de</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Para</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Pais de Origen</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Dirección</th>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<h2>IV. Datos del Producto</h2>
+<div>
+    <table class="tabla-info-empresa">
+        <tbody>
+            <tr>
+                <th colspan="2">Nombre comercial</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">Nombre genérico</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">Forma Farmaceútica</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">Concentración (p.a.)</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">Vía de administración</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th>Acción terapeútica</th>
+                <td></td>
+                <th>Tipo de venta</th>
+                <td></td>                
+            </tr>
+            <tr>
+                <th>Conservación</th>
+                <td></td>
+                <th>Periódo de validez</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th colspan="2">Especificación del envase</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">Envase clínico</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">Aval de la C.F.N.</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">N° de Registro Sanitario</th>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <th colspan="2">N° de Certificado de Control de Calidad</th>
+                <td colspan="2"></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
