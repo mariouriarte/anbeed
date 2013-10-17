2839a261d62738c730c10b3159486ce77253de26
Jose Vasquez <jyva.00@gmail.com>
2013-10-17 15:36:38 -0400

    Se agrego el plugin q se usara para la redimension de las imagenes

--- apps/portal/modules/perfil/templates/_verfoto.php
+++ apps/portal/modules/perfil/templates/_verfoto.php
@@ -6,5 +6,13 @@ if($foto == null || $foto == '')
 
 ?>
 <div class="foto_edit">
+    <?php
+    
+    $thumbnail = new sfThumbnail(60, 75, false, true, 75, 'sfImageMagickAdapter', array('method' => 'shave_all'));
+    $thumbnail->loadFile("images/users/$foto");
+    $thumbnail->save('images/users/new.png', 'image/png');
+    ?>
+    
+    
     <?php echo image_tag(public_path("images/users/$foto"), 'alt=FOTO size=336x336');?>
 </div>
