ce57b632416c91475148a01515eb4f35646c0254
Jose Vasquez <jyva.00@gmail.com>
2013-10-30 15:09:02 -0400

    Esta terminado lo del catalogo del producto... un poco de css falta pero ahora es innecesario

--- apps/web/modules/catalogo/templates/_list_td_tabular.php
+++ apps/web/modules/catalogo/templates/_list_td_tabular.php
@@ -1,12 +1,15 @@
 <div class="web_producto">
     <div class="web_foto">
-        <?php echo image_tag(public_path("images/catalogo/small/".$producto_web->getFoto().""), 'alt_image='.$producto_web->getNombre().' size=150x150');?>
+        <?php echo link_to(image_tag(public_path("images/catalogo/small/".$producto_web->getFoto().""),
+                'alt_image='.$producto_web->getNombre().' size=150x150'), 
+                "catalogo/edit?id=".$producto_web->getId());
+        ?>
     </div>
     <div class="web_titulo">
-        <?php echo $producto_web->getNombre() ?>
+        <?php echo link_to($producto_web->getNombre(), "catalogo/edit/".$producto_web->getId().'/id') ?>
     </div>
     <div class="web_precio">
-        <?php echo $producto_web->getPrecio()." Bs." ?>
+        <?php echo link_to($producto_web->getPrecio()." Bs." , 'catalogo/edit/'.$producto_web->getId().'/id') ?>
     </div>        
 </div>        
        
