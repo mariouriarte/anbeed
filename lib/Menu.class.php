3ee7af4162e7f99118553b4c9b87aea5f46924c4
rolandovaldir <valdir.cadima@gmail.com>
2013-11-05 23:16:43 -0400

    Arreglado el bug del login al sistema por el portal

--- lib/Menu.class.php
+++ lib/Menu.class.php
@@ -111,7 +111,6 @@ class Menu
         $html = '
             <ul class="menu header-menu">
                 <li class="first-list"><a href="/cliente' . $env . '.php"><span>Portal</span></a></li>
-                '. ($user->hasCredential('admin') ? $menu_admin : '') .'
                 <li class="first-list"><a href="/web'. $env . '.php"><span>Home</span></a></li>
                 
             </ul>';
