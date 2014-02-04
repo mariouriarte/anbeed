a1300568dbee04024ff33d6ebb443058a01363fa
git <valdir.cadima@gmail.com>
2014-02-03 22:46:32 -0400

    Cambiando estilo de servicios

--- apps/web/modules/home/templates/importadoraMedicamentosSuccess.php
+++ apps/web/modules/home/templates/importadoraMedicamentosSuccess.php
@@ -4,41 +4,45 @@
 <div class="wide_content_services">
     <img id="etiqueta" src="/images/web/sociedad.jpg"/>
     <h2>Requisitos para Apertura de Empresas Importadoras de Medicamentos S.R.L. ó S.A.</h2>
-    <div class="imagen-presentacion">
-        <img id="central" src="/images/web/medicinas.gif"/>
-    </div>
-    <div class="requisitos">
-        <h3>Documentación legal de la empresa:</h3>
+    <div class="sub">
+        <div class="imagen-presentacion">
+            <img id="servicio" src="/images/web/medicinas.gif"/>
+        </div>
+        <div class="requisitos-dos">
+            <h3>Documentación legal de la empresa:</h3>
 
-        <div class="lista_requisitos">
-            <ol>
-                <li><p>Matricula de <b>FUNDEMPRESA</b> emitida por la Cámara de Industria y <b>Comercio Padrón Municipal</b> emitído por la Alcaldía Municipal Fotocopia Legalizada</p></li>
-                <li><p>Fotocopia de <b>NIT</b></p></li>
-                <li><p>Fotocopia Legalizada de la <b>Constitución de la Empresa</b></p></li>
-                <li><p>Fotocopia Legalizada del <b>Poder de Representante Legal</b></p></li>
-                <li><p><b>Carnet de Identidad</b> del Representante Legal</p></li>
-            </ol>
-        </div>        
-    </div>
-    <div class="requisitos">
-        <h3>Documentación Regente Farmacéutica:</h3> 
-        <div class="lista_requisitos">
-            <ol>
-                <li><p> Fotocopia Legalizada <b>Titulo en Provisión Nacional</b> (PERFIL PROFESIONAL FARMACEUTICO)</p></li>
-                <li><p> Fotocopia <b>Matricula profesional</b></p></li>
-                <li><p> Fotocopia <b>Carnet de Colegiada</b></p></li>
-                <li><p> Fotocopia <b>Carnet de Identidad</b></p></li>
-                <li><p> <b>Certificado de Compatibilidad Horaria</b> emitida por el SEDES por tiempo completo</p></li>
-                <li><p> <b>Contrato de trabajo Visado</b> en el Ministerio de Trabajo</p></li>
-            </ol>
+            <div class="lista_requisitos">
+                <ol>
+                    <li><p>Matricula de <b>FUNDEMPRESA</b> emitida por la Cámara de Industria y <b>Comercio Padrón Municipal</b> emitído por la Alcaldía Municipal Fotocopia Legalizada</p></li>
+                    <li><p>Fotocopia de <b>NIT</b></p></li>
+                    <li><p>Fotocopia Legalizada de la <b>Constitución de la Empresa</b></p></li>
+                    <li><p>Fotocopia Legalizada del <b>Poder de Representante Legal</b></p></li>
+                    <li><p><b>Carnet de Identidad</b> del Representante Legal</p></li>
+                </ol>
+            </div>        
         </div>
     </div>
-    <div class="requisitos">
-        <div class="lista_requisitos">
-            <h3>Arancel Ministerio de Salud:</h3>
-            <ol>
-                <li><p>Pago de 10.000 Bs al Banco Central de Bolivia a la cuenta 3G-300 del Ministerio de Salud y deportes por concepto de apertura de empresa importadora</p></li>
-            </ol>
+    <div class="sub">
+        <div class="requisitos-dos">
+            <h3>Documentación Regente Farmacéutica:</h3> 
+            <div class="lista_requisitos">
+                <ol>
+                    <li><p> Fotocopia Legalizada <b>Titulo en Provisión Nacional</b> (PERFIL PROFESIONAL FARMACEUTICO)</p></li>
+                    <li><p> Fotocopia <b>Matricula profesional</b></p></li>
+                    <li><p> Fotocopia <b>Carnet de Colegiada</b></p></li>
+                    <li><p> Fotocopia <b>Carnet de Identidad</b></p></li>
+                    <li><p> <b>Certificado de Compatibilidad Horaria</b> emitida por el SEDES por tiempo completo</p></li>
+                    <li><p> <b>Contrato de trabajo Visado</b> en el Ministerio de Trabajo</p></li>
+                </ol>
+            </div>
+        </div>
+        <div class="requisitos-dos">
+            <div class="lista_requisitos">
+                <h3>Arancel Ministerio de Salud:</h3>
+                <ol>
+                    <li><p>Pago de 10.000 Bs al Banco Central de Bolivia a la cuenta 3G-300 del Ministerio de Salud y deportes por concepto de apertura de empresa importadora</p></li>
+                </ol>
+            </div>
         </div>
     </div>
 </div>
@@ -46,45 +50,48 @@
 <div class="wide_content_services">
     <img id="etiqueta" src="/images/web/unipersonal.jpg"/>
     <h2>Requisitos para Apertura de Empresas Importadoras de Medicamentos UNIPERSONAL</h2>
-    <div class="imagen-presentacion">
-        <img id="central" src="/images/web/injectable.jpg"/>
-    </div>
-    <div class="requisitos">
-        <h3>Documentacion Legal de la Empresa:</h3>
-        <div class="lista_requisitos">
-            <ol>
-                <li><p> Matricula de <b>FUNDEMPRESA</b> emitida por la Cámara de Industria y <b>Comercio Padrón Municipal</b> emitído por la Alcaldía Municipal Fotocopia Legalizada</p></li>
-                <li><p> Fotocopia de <b>NIT</b> </p></li>
-                <li><p> <b>Carnet de Identidad</b> del Representante Legal</p></li>
-            </ol>
+    <div class="sub">
+        <div class="imagen-presentacion">
+            <img id="servicio" src="/images/web/injectable.jpg"/>
+        </div>
+        <div class="requisitos-dos">
+            <h3>Documentacion Legal de la Empresa:</h3>
+            <div class="lista_requisitos">
+                <ol>
+                    <li><p> Matricula de <b>FUNDEMPRESA</b> emitida por la Cámara de Industria y <b>Comercio Padrón Municipal</b> emitído por la Alcaldía Municipal Fotocopia Legalizada</p></li>
+                    <li><p> Fotocopia de <b>NIT</b> </p></li>
+                    <li><p> <b>Carnet de Identidad</b> del Representante Legal</p></li>
+                </ol>
+            </div>
         </div>
     </div>
-
-    <div class="requisitos">
-        <h3>Documentacion Regente Farmaceutica:</h3>
-        <div class="lista_requisitos">
-            <ol>
-                <li><p> Fotocopia Legalizada <b>Titulo en Provisión Nacional</b> (PERFIL PROFESIONAL FARMACEUTICO)</p></li>
-                <li><p> Fotocopia <b>Matricula profesional</b></p></li>
-                <li><p> Fotocopia <b>Carnet de Colegiada</b></p></li>
-                <li><p> Fotocopia <b>Carnet de Identidad</b></p></li>
-                <li><p> <b>Certificado de Compatibilidad Horaria</b> emitida por el SEDES por tiempo completo</p></li>
-                <li><p> <b>Contrato de trabajo Visado</b> en el Ministerio de Trabajo</p></li>
-            </ol>
+    <div>
+        <div class="requisitos-dos">
+            <h3>Documentacion Regente Farmaceutica:</h3>
+            <div class="lista_requisitos">
+                <ol>
+                    <li><p> Fotocopia Legalizada <b>Titulo en Provisión Nacional</b> (PERFIL PROFESIONAL FARMACEUTICO)</p></li>
+                    <li><p> Fotocopia <b>Matricula profesional</b></p></li>
+                    <li><p> Fotocopia <b>Carnet de Colegiada</b></p></li>
+                    <li><p> Fotocopia <b>Carnet de Identidad</b></p></li>
+                    <li><p> <b>Certificado de Compatibilidad Horaria</b> emitida por el SEDES por tiempo completo</p></li>
+                    <li><p> <b>Contrato de trabajo Visado</b> en el Ministerio de Trabajo</p></li>
+                </ol>
+            </div>
         </div>
-    </div>
-    <div class="requisitos">
-        <h3>Arancel Ministerio de Salud:</h3>
-        <div class="lista_requisitos">
-            <ol>
-                <li><p>
-                        Pago de 10.000 Bs al Banco Central de Bolivia a la cuenta 3G-300 del Ministerio de Salud y deportes por concepto de apertura de empresa importadora
-                    </p>
-                </li>
-            </ol>
+        <div class="requisitos-dos">
+            <h3>Arancel Ministerio de Salud:</h3>
+            <div class="lista_requisitos">
+                <ol>
+                    <li><p>
+                            Pago de 10.000 Bs al Banco Central de Bolivia a la cuenta 3G-300 del Ministerio de Salud y deportes por concepto de apertura de empresa importadora
+                        </p>
+                    </li>
+                </ol>
 
-            <h4>Nota.</h4>
-            <h4>Para el trámite debe enviarse los datos de ubicación de la empresa, Nº telefónico, Nº de fax y email.</h4>
+                <h4>Nota.</h4>
+                <h4>Para el trámite debe enviarse los datos de ubicación de la empresa, Nº telefónico, Nº de fax y email.</h4>
+            </div>
         </div>
     </div>
 </div>
