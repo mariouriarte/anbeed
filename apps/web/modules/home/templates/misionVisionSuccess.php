89621c758226c701b107c34c80c46abc84382de8
git <valdir.cadima@gmail.com>
2014-02-04 01:29:53 -0400

    Adicionado stilo de shadow a los parrafos

--- apps/web/modules/home/templates/misionVisionSuccess.php
+++ apps/web/modules/home/templates/misionVisionSuccess.php
@@ -6,7 +6,7 @@
     <div  class="moduletable_ol_light">
     <h3>Misión</h3>
     
-    <p>
+    <p id="shadow-ligth">
         Lograr convertirnos en una empresa de servicios integrados Líder en el mercado nacional e internacional con capacidad idónea, técnica, profesional, orientada y comprometida con sus clientes para el cumplimiento de sus objetivos.
         <img src="/images/web/mision.jpg">
     </p>
@@ -18,7 +18,7 @@
     <div class="moduletable_ol_light">
     <h3>Visión</h3>
     
-    <p>
+    <p id="shadow-ligth">
         Constituirnos en una empresa reconocida en el ámbito laboral por su capacidad de ofrecer servicios integrados hasta cualquier parte del mundo y en consecuencia plantear  las soluciones y/o alternativas oportunas y efectivas para lograr satisfacer plenamente las necesidades de sus clientes.
         <img src="/images/web/vision.jpg">
     </p>
--- apps/web/modules/home/templates/quienesSomosSuccess.php
+++ apps/web/modules/home/templates/quienesSomosSuccess.php
@@ -6,11 +6,11 @@
         <div class="moduletable_ol_light">
             <h3>Acerca de Nosotros</h3>
             <img src="/images/web/foto_grupal.png">
-            <p>
+            <p id="shadow-ligth">
                 La empresa "AN.BE.ED. SRL" se encuentra funcionando exitosamente en la ciudad de La Paz desde  el  año 2005, brindando servicios de asesoría legal, financiera y especialmente un servicio personalizado en regulación farmacéutica, importación y distribución de  medicamentos, insumos médicos, cosméticos y otros que se encuentran dentro del marco legal de la Ley del Medicamento 1737 en Bolivia  y su Decreto reglamentario 25235.
             </p>
             
-            <p>
+            <p id="shadow-ligth">
                 De acuerdo a seguimiento efectuado en el mercado de nuestro país, se pudo evidenciar la escasez de Instituciones que se dediquen a realizar tales servicios, razón por la que surge la iniciativa de crear ANBEED, para precisamente cubrir en parte la demanda de los servicios requeridos y relacionados a nuestra actividad.
             </p>            
         </div>
@@ -19,10 +19,10 @@
         <div class="moduletable_ol_light">
             <h3>Por qué somos diferentes </h3>
             <img src="/images/web/pq_somos_diferentes.jpg">
-            <p>
+            <p id="shadow-ligth">
                 Por ser especialistas en trámites de Inscripción de registros sanitarios y despachos aduaneros ante el Ministerio de Salud y Deportes, contamos con personal capacitado y actualizado tecnológicamente utilizando un moderno sistema que facilita y agiliza nuestros procesos. 
             </p>
-            <p>
+            <p id="shadow-ligth">
                 Además ofrecemos a nuestros clientes un servicio virtual, mediante el cual puede solicitar y obtener información sobre nuestros servicios, hacer el seguimiento de sus trámites y visualizar los productos que ofrecemos.
             </p>
         </div>
