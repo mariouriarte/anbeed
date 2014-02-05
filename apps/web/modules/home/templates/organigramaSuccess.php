05e0381ccf54f6412ca0546fb5c7415c6084d398
Rolando <valdir.cadimo@gmail.com>
2014-02-04 23:24:11 -0400

    afinacion organigrama, fotos mas grandes
    raya de titulo uniforme

--- apps/web/modules/home/templates/organigramaSuccess.php
+++ apps/web/modules/home/templates/organigramaSuccess.php
@@ -3,45 +3,45 @@
 </script>
 <div class="center_content_pages">
     
-    <h2 id="title-org">Personal Activo</h2>
+    <h2 id="title-org">Recursos Humanos</h2>
     
     <div class="wide-organigrama-logo">
         <!--<img border="0" alt="" src="/images/logo.jpg">-->
     </div>
     <div id="org-panel">
     <div class="wide_organigrama">
-        <div class="foto-personal"><img alt="" src="/images/web/organigrama/ana_ruth.jpg"></div>
-        <p><span class="org-nombre-personal">Dra. Ana Ruth Gutierrez Poveda</span></p>
+        <div class="foto-personal"><img alt="" src="/images/web/organigrama/IMG_1071.jpg"></div>
+        <span class="org-nombre-personal"><p>Dra. Ana Ruth Gutierrez Poveda</p></span>
         <p><span class="org-cargo">Gerente General</span></p>
     </div>
     <div class="wide_organigrama">
-        <div class="foto-personal"><img alt="" src="/images/web/organigrama/renato.jpg"></div>
-        <p><span class="org-nombre-personal">Renato Vargas</span></p>
+        <div class="foto-personal"><img alt="" src="/images/web/organigrama/IMG_1096.jpg"></div>
+        <span class="org-nombre-personal"><p>Renato Vargas</p></span>
         <p><span class="org-cargo">Técnico en Registro Sanitario</span></p>
     </div>
     <div class="wide_organigrama">
-        <div class="foto-personal"><img alt="" src="/images/web/organigrama/huascar.jpg"></div>
-        <p><span class="org-nombre-personal">Huascar Arce</span></p>
+        <div class="foto-personal"><img alt="" src="/images/web/organigrama/IMG_1039.jpg"></div>
+        <span class="org-nombre-personal"><p>Huascar Arce</p></span>
         <p><span class="org-cargo">Técnico en Despacho Aduanero</span></p>
     </div>
     <div class="wide_organigrama">
-        <div class="foto-personal"><img alt="" src="/images/web/organigrama/christian.jpg"></div>
-        <p><span class="org-nombre-personal">Christian Paukner</span></p>
+        <div class="foto-personal"><img alt="" src="/images/web/organigrama/IMG_1043.jpg"></div>
+        <span class="org-nombre-personal"><p>Christian Paukner</p></span>
         <p><span class="org-cargo">Responsable de Almacén y Cobranzas</span></p>
     </div>
     <div class="wide_organigrama">
-        <div class="foto-personal"><img alt="" src="/images/web/organigrama/narda.jpg"></div>
-        <p><span class="org-nombre-personal">Narda Magaly Gutierrez</span></p>
+        <div class="foto-personal"><img alt="" src="/images/web/organigrama/IMG_1046.jpg"></div>
+        <span class="org-nombre-personal"><p>Narda Magaly Gutierrez</p></span>
         <p><span class="org-cargo">Responsable de Correspondencia</span></p>
     </div>
     <div class="wide_organigrama">
-        <div class="foto-personal"><img alt="" src="/images/web/organigrama/marco.jpg"></div>
-        <p><span class="org-nombre-personal">Marco Antonio Martinez</span></p>
+        <div class="foto-personal"><img alt="" src="/images/web/organigrama/IMG_1078.jpg"></div>
+        <span class="org-nombre-personal"><p>Marco Antonio Martinez</p></span>
         <p><span class="org-cargo">Responsable de Ventas</span></p>
     </div>
     <div class="wide_organigrama">
-        <div class="foto-personal"><img alt="" src="/images/web/organigrama/carlos.jpg"></div>
-        <p><span class="org-nombre-personal">Carlos Pinto Zabala</span></p>
+        <div class="foto-personal"><img alt="" src="/images/web/organigrama/IMG_1093.jpg"></div>
+        <span class="org-nombre-personal"><p>Carlos Pinto Zabala</p></span>
         <p><span class="org-cargo">Ventas</span></p>
     </div>
     
