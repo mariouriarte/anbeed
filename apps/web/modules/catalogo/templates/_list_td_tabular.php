747a1df3b957b4740beb9b27455b72892a2fe44a
Jose Vasquez <jyva.00@gmail.com>
2014-02-05 14:06:28 -0400

    Merge concluido cpn wen-menu con los arrelgos del header, y en el catalogo se elimino el link para ver detalle del producto... momentaneamente.

--- apps/web/modules/catalogo/templates/_list_td_tabular.php
+++ apps/web/modules/catalogo/templates/_list_td_tabular.php
@@ -1,12 +1,17 @@
 <div class="web_producto">
     <div class="web_foto">
-        <?php echo link_to(image_tag(public_path("images/catalogo/small/".$producto_web->getFoto().""),
-                'alt_image='.$producto_web->getNombre().' size=150x150'), 
-                "catalogo/edit?id=".$producto_web->getId());
+        <?php 
+        echo link_to(image_tag(public_path("images/catalogo/small/".$producto_web->getFoto().""),
+                'alt_image='.$producto_web->getNombre().' size=150x150'));
+        
+//        echo link_to(image_tag(public_path("images/catalogo/small/".$producto_web->getFoto().""),
+//                'alt_image='.$producto_web->getNombre().' size=150x150'), 
+//                "catalogo/edit?id=".$producto_web->getId());
         ?>
     </div>
     <div class="web_titulo">
-        <?php echo link_to($producto_web->getNombre(), "catalogo/edit/".$producto_web->getId().'/id') ?>
+        <?php echo $producto_web->getNombre(); ?>
+        <?php // echo link_to($producto_web->getNombre(), "catalogo/edit/".$producto_web->getId().'/id') ?>
     </div>
     <div class="web_precio">
         <?php // echo link_to($producto_web->getPrecio()." Bs." , 'catalogo/edit/'.$producto_web->getId().'/id') ?>
